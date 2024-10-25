b86393e7379d1e311406721091ecf281a83f5d3c
MarsusHusus <Marvin.Huber@edu.gbssg.ch>
2024-10-25 08:42:24 +0200

    commit3

--- index.php
+++ index.php
@@ -1,1 +1,67 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>index.php</p>
+    <?php 
+    $get = $_GET;
+    $post = $_POST;
+
+    echo '<pre>';
+    print_r($get);
+    
+    echo '<pre>';
+    print_r($post);
+    ?>
+</body>
+</html>
+
+
+
+<!-- 
+
+PHP Tag
+
+echo
+
+Variabeln
+$a = 0;
+keine zahl nach dollar zeichen
+
+$c = $a + $b
+echo "wert: $c";
+
+$Name = $Vorname . " " . $Nachname;
+
+if () echo "";
+
+if () {
+
+} elseif () {
+ 
+} else {
+ 
+}
+
+switch()
+case
+echo
+break;
+default
+
+for ()
+
+while ()
+
+do () {
+} while
+
+try catch finally
+
+
+
+-->
